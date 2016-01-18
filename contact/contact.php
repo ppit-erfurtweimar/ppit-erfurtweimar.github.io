c8da2ce494dbd28b3c379df62365f2dfbb71cb63
ppit-erfurtweimar <efwethueringen@gmail.com>
2016-01-18 23:57:34 +0100

    small fixed and welcome added

--- contact/contact.php
+++ contact/contact.php
@@ -5,11 +5,11 @@
 	$subject = $_REQUEST[ 'contact-subject' ];
 	$message = $_REQUEST[ 'contact-message' ];
 	$mail_subject = $subject . "( Customer Contact )";
-	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
+	$message = "Your Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
 	
 	/* Replace YOUR_MAIL With Your Mail Address inside '' */
 	if ( mail( 'efwethueringen@gmail.com', $mail_subject, $message, "From:" . $email ) ) {
-		echo "Thank you <strong>$name</strong> for contacting with us !!!";
+		echo "Terima kasih loh <strong>$name</strong> ,kamu luar biasa!!!";
 	} else {
 		echo "Error in sending message !!! Please try again";
 	}
